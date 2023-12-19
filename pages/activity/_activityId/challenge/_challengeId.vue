d674202e4caf792969c1168229703615086dbba4
Josep Crespo Santacreu <josep.crespo.santacreu@gmail.com>
2023-12-19 04:52:56 +0000

    Basic dynamic Challange page.

---
+++ pages/activity/_activityId/challenge/_challengeId.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <h1>Actividad: {{ activityId }}</h1>
+        <h2>Reto: {{ challengeId }}</h2>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'ChallengePage',
+  async asyncData({ params }) {
+    const { activityId, challengeId } = params
+    return await { activityId, challengeId }
+  }
+}
+</script>
