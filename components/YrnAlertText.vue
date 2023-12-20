43d403c2dc2f252d90373c86a2a42e447c5c22d4
Josep Crespo Santacreu <josep.crespo.santacreu@gmail.com>
2023-12-20 00:45:40 +0000

    New YrnAlertText component.

---
+++ components/YrnAlertText.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-row class="yrn-alert-text">
+    <v-col>
+      <v-alert
+        :border="border"
+        :color="color"
+      >
+        <p
+          v-for="(paragraph, index) in paragraphs"
+          :key="index"
+        >
+          {{ paragraph }}
+        </p>
+      </v-alert>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'YrnAlertText',
+  props: {
+    border: {
+      default: 'left',
+      required: false,
+      type: String
+    },
+    color: {
+      default: 'blue-grey',
+      required: false,
+      type: String
+    },
+    paragraphs: {
+      required: true,
+      type: Array
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.yrn-alert-text {
+  // Custom style
+}
+</style>
