0f0ad3413660da5c229e28683d727695e4cf3928
Josep Crespo Santacreu <josep.crespo.santacreu@gmail.com>
2023-12-21 10:01:15 +0000

    New YrnExercise component.

---
+++ components/YrnExercise.vue
@@ -0,0 +1,81 @@
+<template>
+  <v-row class="yrn-exercise">
+    <v-col>
+      <h2 class="mt-8 mb-4">
+        {{ statement }}
+      </h2>
+      <yrn-exercise-sections-group-progress
+        class="mb-2"
+        :current="exerciseSectionsWellSolved.length"
+        :total="exerciseSections.length"
+      />
+      <yrn-exercise-sections-group
+        :activity-id="activityId"
+        :challenge-id="challengeId"
+        :exercise="exercise"
+      />
+      <yrn-exercise-sections-group-progress
+        class="mt-2"
+        :current="exerciseSectionsWellSolved.length"
+        :total="exerciseSections.length"
+      />
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'YrnExercise',
+  props: {
+    activityId: {
+      required: true,
+      type: [Number, String]
+    },
+    challengeId: {
+      required: true,
+      type: [Number, String]
+    },
+    exerciseId: {
+      required: true,
+      type: [Number, String]
+    },
+  },
+  computed: {
+    exercise() {
+      return this.getExercise(this.activityId, this.challengeId, this.exerciseId)
+    },
+    exerciseSections() {
+      return this.exercise?.sections ?? []
+    },
+    exerciseSectionsWellSolved() {
+      return this.getExerciseSectionsWellSolved(
+        this.activityId,
+        this.challengeId,
+        this.exerciseId
+      )
+    },
+    statement() {
+      return this.exercise?.statement
+    }
+  },
+  methods: {
+    getExercise(activityId, challengeId, exerciseId) {
+      return this.$store.getters['learningUnit/getExercise'](
+        activityId,
+        challengeId,
+        exerciseId
+      )
+    },
+    getExerciseSections() {
+      return this.exercise?.sections ?? []
+    },
+    getExerciseSectionsWellSolved(activityId, challengeId, exerciseId) {
+      return this.$store.getters['learningUnit/getExerciseSectionsWellSolved'](
+        activityId,
+        challengeId,
+        exerciseId
+      )
+    }
+  }
+}
+</script>
