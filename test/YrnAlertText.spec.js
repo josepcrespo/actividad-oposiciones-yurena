0a5c2681f4abc7742dbd7faaad9e30f2b42727c4
Josep Crespo Santacreu <josep.crespo.santacreu@gmail.com>
2023-12-28 18:04:55 +0000

    Code refactor an new test added.

--- test/YrnAlertText.spec.js
+++ test/YrnAlertText.spec.js
@@ -10,38 +10,69 @@ const localVue
 localVue.use(Vuetify)
 
 describe('YrnAlertText', () => {
+  const propsData = {
+    paragraphs: {
+      en: [
+        'English paragraph 1',
+        'English paragraph 2'
+      ],
+      es: [
+        'Párrafo en español 1',
+        'Párrafo en español 2'
+      ],
+    },
+  }
+  const mocks = { $i18n: { locale: 'es' } }
+
   let vuetify
 
   beforeEach(() => {
     vuetify = new Vuetify()
   })
 
-  test('is a Vue instance', () => {
+  test('is a Vue instance and renders correctly.', () => {
     const wrapper = mount(YrnAlertText, {
       localVue,
       vuetify,
-      propsData: {
-        paragraphs: {
-          en: [
-            'English paragraph 1',
-            'English paragraph 2'
-          ],
-          es: [
-            'Párrafo en español 1',
-            'Párrafo en español 2'
-          ]
-        }
-      },
-      mocks: {
-        $i18n: {
-          locale: 'es'
-        },
-      }
+      propsData,
+      mocks
     })
 
     // Renders correctly
     expect(wrapper.vm).toBeTruthy()
-    // It has the main component class
+  })
+
+  test('It has the component\'s unique root class.', () => {
+    const wrapper = mount(YrnAlertText, {
+      localVue,
+      vuetify,
+      propsData,
+      mocks
+    })
+
     expect(wrapper.classes('yrn-alert-text')).toBe(true)
+  })
+
+  test('computed property `localizedParagraphs` returns the correct value.', () => {
+    const wrapper = mount(YrnAlertText, {
+      localVue,
+      vuetify,
+      propsData,
+      mocks
+    })
+    const paragraphs = {
+      es: [
+        'Prueba párrafo 1',
+        'Prueba párrafo 2'
+      ]
+    }
+
+    // Mock the computed property
+    wrapper.setData({ paragraphs })
+
+    expect(wrapper.vm.localizedParagraphs).toEqual([
+      paragraphs.es[0],
+      paragraphs.es[1]
+    ])
   })
 })
