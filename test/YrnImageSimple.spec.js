1cb0e833cb17fd2bd54f87d89da93c81bb61215f
Josep Crespo Santacreu <josep.crespo.santacreu@gmail.com>
2023-12-28 19:23:05 +0000

    Update YrnImageSimple.spec.js

--- test/YrnImageSimple.spec.js
+++ test/YrnImageSimple.spec.js
@@ -27,34 +27,22 @@ describe('YrnImageSimple', () => {
   })
 
   test('is a Vue instance and renders correctly.', () => {
-    const wrapper = mount(YrnImageSimple, {
-      localVue,
-      vuetify,
-      propsData,
-      mocks
-    })
+    const wrapper
+      = mount(YrnImageSimple, { localVue, vuetify, propsData, mocks })
 
     expect(wrapper.vm).toBeTruthy()
   })
 
   test('It has the component\'s unique root class.', () => {
-    const wrapper = mount(YrnImageSimple, {
-      localVue,
-      vuetify,
-      propsData,
-      mocks
-    })
+    const wrapper
+      = mount(YrnImageSimple, { localVue, vuetify, propsData, mocks })
 
     expect(wrapper.classes(rootClass)).toBe(true)
   })
 
   test('computed property `localizedAlt` returns the correct value.', () => {
-    const wrapper = mount(YrnImageSimple, {
-      localVue,
-      vuetify,
-      propsData,
-      mocks
-    })
+    const wrapper
+      = mount(YrnImageSimple, { localVue, vuetify, propsData, mocks })
     const alt = {
       en: 'Alternative text 1',
       es: 'Texto alternativo 1'
