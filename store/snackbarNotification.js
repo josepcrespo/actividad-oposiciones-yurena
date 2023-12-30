d0e8846218a96696532f729de6b6572be6d39365
Josep Crespo Santacreu <josep.crespo.santacreu@gmail.com>
2023-12-30 06:02:09 +0000

    Added missing translations for other languages.

--- store/snackbarNotification.js
+++ store/snackbarNotification.js
@@ -9,13 +9,57 @@ export const state = () => ({
   showAction: true,
   text: {
     defaults: {
-      es: {
-        error: 'Mmm… esto no es lo que esperaba… 😔',
-        success: '¡Bien hecho!'
+      ar: {
+        error: '😔 ممم… هذا ليس ما تتوقعه…',
+        success: 'عمل رائع!'
       },
       ca: {
         error: 'Mmm… açò no és el que esperava… 😔',
         success: 'Ben fet!'
+      },
+      de: {
+        error: 'Mmm... das ist nicht, was ich erwartet habe... 😔',
+        success: 'Gut gemacht!'
+      },
+      en: {
+        error: 'Mmm... this is not what I expected... 😔',
+        success: 'Well done!'
+      },
+      es: {
+        error: 'Mmm… esto no es lo que esperaba… 😔',
+        success: '¡Bien hecho!'
+      },
+      fr: {
+        error: 'Mmm... ce n\'est pas ce à quoi je m\'attendais... 😔',
+        success: 'Bien joué !'
+      },
+      gl: {
+        error: 'Mmm... iso non é o que esperaba... 😔',
+        success: 'Ben feito!'
+      },
+      it: {
+        error: 'Mmm... questo non è quello che mi aspettavo... 😔',
+        success: 'Ben fatto!'
+      },
+      ja: {
+        error: 'うーん... これは予想していたものと違いますね... 😔',
+        success: 'よくやった！'
+      },
+      pt: {
+        error: 'Mmm... isto não é o que eu esperava... 😔',
+        success: 'Bem feito!'
+      },
+      ro: {
+        error: 'Mmm... asta nu e ce mă așteptam să văd... 😔',
+        success: 'Foarte bine!'
+      },
+      ru: {
+        error: 'Ммм... это не то, что я ожидал... 😔',
+        success: 'Отлично!'
+      },
+      zh: {
+        error: '嗯... 这不是我预期的... 😔',
+        success: '做得好！'
       }
     },
     toShow: ''
