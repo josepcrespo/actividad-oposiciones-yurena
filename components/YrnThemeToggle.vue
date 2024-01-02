aa426226abc421feeb0bba34671a57952cfce1e8
Josep Crespo Santacreu <josep.crespo.santacreu@gmail.com>
2024-01-02 04:06:06 +0000

    Removed unnecessary semicolon.

--- components/YrnThemeToggle.vue
+++ components/YrnThemeToggle.vue
@@ -74,7 +74,7 @@ export default {
   name: 'YrnThemeToggle',
   methods: {
     toggleTheme() {
-      this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+      this.$vuetify.theme.dark = !this.$vuetify.theme.dark
     }
   }
 }
