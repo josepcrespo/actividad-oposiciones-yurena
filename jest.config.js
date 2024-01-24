50169e7f9919726de90bfb4d27e8a282896f73cb
Josep Crespo Santacreu <josep.crespo.santacreu@gmail.com>
2024-01-24 00:59:57 +0000

    Alphabetically sorted jest.config.js properties for improved readability and maintainability.

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,18 @@
 module.exports = {
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1',
-    '^~/(.*)$': '<rootDir>/$1',
-    '^vue$': 'vue/dist/vue.common.js',
-  },
-  moduleFileExtensions: ['js', 'vue', 'json'],
-  transform: {
-    '^.+\\.js$': 'babel-jest',
-    '.*\\.(vue)$': 'vue-jest',
-  },
   collectCoverage: true,
   collectCoverageFrom: [
     '<rootDir>/components/**/*.vue',
     '<rootDir>/pages/**/*.vue',
   ],
+  moduleFileExtensions: ['js', 'vue', 'json'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+    '^~/(.*)$': '<rootDir>/$1',
+    '^vue$': 'vue/dist/vue.common.js',
+  },
   testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+    '.*\\.(vue)$': 'vue-jest',
+  }
 }
