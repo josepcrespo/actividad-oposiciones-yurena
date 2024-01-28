8a3b136436276318e764adde804f36b61907d291
Josep Crespo Santacreu <josep.crespo.santacreu@gmail.com>
2024-01-28 08:42:03 +0000

    New componente and test page.

---
+++ components/YrnCollectionCard.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-card
+    class="yrn-collection-card mx-auto pb-1 my-4"
+    elevation="5"
+    max-width="320"
+    rounded="xl"
+  >
+    <v-img
+      :src="image"
+      height="200px"
+    />
+    <v-card-title>
+      {{ title }}
+    </v-card-title>
+    <v-card-subtitle>
+      {{ subtitle }}
+    </v-card-subtitle>
+    <v-divider></v-divider>
+    <v-card-text>
+      {{ text }}
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    image: {
+      required: true,
+      type: String
+    },
+    subtitle: {
+      required: true,
+      type: String
+    },
+    text: {
+      required: true,
+      type: String
+    },
+    title: {
+      required: true,
+      type: String
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.yrn-collection-card {
+  .v-card {
+    &__text {
+      height: 200px;
+      overflow-y: auto;
+    }
+  }
+}
+</style>
---
+++ pages/test4.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-row>
+    <v-col class="d-flex justify-center">
+      <yrn-collection-card
+        class="mt-6"
+        image="/img/actividad-1/reto-3/1-retama.jpg"
+        subtitle="Spartocytisus supranubius"
+        text="Spartocytisus supranubius o Cytisus supranubius es una especie de retamas de flores blancas nativa de las Islas Canarias, donde es conocida como retama del Teide. Esta especie crece en zonas de alta montaña, como en Las Cañadas del Teide, en la isla de Tenerife y en las zonas montañosas de La Palma."
+        title="Retama del Teide"
+      />
+      <yrn-collection-card
+        class="mt-6"
+        image="/img/actividad-1/reto-3/1-retama.jpg"
+        subtitle="Spartocytisus supranubius"
+        text="Spartocytisus supranubius o Cytisus supranubius es una especie de retamas de flores blancas nativa de las Islas Canarias, donde es conocida como retama del Teide. Esta especie crece en zonas de alta montaña, como en Las Cañadas del Teide, en la isla de Tenerife y en las zonas montañosas de La Palma."
+        title="Retama del Teide"
+      />
+      <yrn-collection-card
+        class="mt-6"
+        image="/img/actividad-1/reto-3/1-retama.jpg"
+        subtitle="Spartocytisus supranubius"
+        text="Spartocytisus supranubius o Cytisus supranubius es una especie de retamas de flores blancas nativa de las Islas Canarias, donde es conocida como retama del Teide. Esta especie crece en zonas de alta montaña, como en Las Cañadas del Teide, en la isla de Tenerife y en las zonas montañosas de La Palma."
+        title="Retama del Teide"
+      />
+    </v-col>
+  </v-row>
+</template>
