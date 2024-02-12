37446a2bdc1c31b36082f4282d8d9bbc41d49b5d
Josep Crespo Santacreu <josep.crespo.santacreu@gmail.com>
2024-02-12 01:47:21 +0000

    Added `vuex-persistedstate` plugin for Vuex state persistence.

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,6 +26,7 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     '~/plugins/i18n.js',
+    '~/plugins/persistedState.client.js',
     '~/plugins/textToSpeech.client.js',
     '~/plugins/utils.client.js'
   ],
---
+++ plugins/persistedState.client.js
@@ -0,0 +1,21 @@
+// https://github.com/robinvdvleuten/vuex-persistedstate/#example-with-nuxtjs
+import createPersistedState from 'vuex-persistedstate'
+
+/**
+ * The default function is a higher-order function that takes in an object
+ * with a 'store' property and returns a function that persists the state
+ * of the Vuex store using the vuex-persistedstate library.
+ *
+ * @param {Object} options - An object containing the 'store' property.
+ * @param {Object} options.store - The Vuex store object.
+ * @returns {Function} - A function that persists the state of the Vuex store.
+ */
+export default ({ store }) => {
+  createPersistedState({
+    key: 'app.vuex.state',
+    paths: [
+      'index',
+      'learningUnit'
+    ]
+  })(store)
+}
