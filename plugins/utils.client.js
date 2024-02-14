60c0f6dd851154c9c1aa0176251ffeb8d4bdd216
Josep Crespo Santacreu <josep.crespo.santacreu@gmail.com>
2024-02-14 05:34:07 +0000

    Added new utility functions.
    New global functions:
    - castIndexToCharacter
    - getRandomBoolean
    - getRandomInt
    
    Added documentation to each function (to the newer and the previous ones).

--- plugins/utils.client.js
+++ plugins/utils.client.js
@@ -1,11 +1,55 @@
 // https://v2.nuxt.com/docs/directory-structure/plugins/#the-plugins-property
 export default ({ app }, inject) => {
-  /* Here we inject the global method '$capitalizeWords' */
+  /**
+   * Capitalize the first letter of each word in a string.
+   * 
+   * @param {string} string - The string to capitalize.
+   * @returns {string} The capitalized string.
+   */
   inject('capitalizeWords', (string) => {
     if (typeof string !== 'string') return ''
     return string.replace(/\b\w/g, match => match.toUpperCase())
   })
 
+  /**
+   * Converts an index to a character.
+   * 
+   * @param {number} index - The index to convert to a character.
+   * @param {boolean} [capitalized=false] - Whether to capitalize the character.
+   * @returns {string} The character.
+   */
+  inject('castIndexToCharacter', (index, capitalized = false) => {
+    return capitalized
+      ? String.fromCharCode(index + 65)
+      : String.fromCharCode(index + 97)
+  })
+
+  /**
+   * Returns a random boolean.
+   * 
+   * @returns {boolean} The random boolean.
+   */
+  inject('getRandomBoolean', () => {
+    return window?.Math?.random() < 0.5
+  })
+
+  /**
+   * Returns a random integer between min (inclusive) and max (inclusive).
+   * 
+   * @param {number} min - The minimum value (inclusive).
+   * @param {number} max - The maximum value (inclusive).
+   * @returns {number} The random integer.
+   */
+  inject('getRandomInt', (min, max) => {
+    return window?.Math?.floor(window?.Math?.random() * (max - min + 1)) + min
+  })
+
+  /**
+   * Shuffles an array.
+   * 
+   * @param {Array} array - The array to shuffle.
+   * @returns {Array} The shuffled array.
+   */
   inject('shuffleArray', (array) => {
     if (!Array.isArray(array)) {
       throw new TypeError('Invalid argument: array must be an array')
@@ -25,6 +69,11 @@ export default ({ app }, inject) => {
     return newArray
   })
 
+  /**
+   * Scrolls to an element with the given selector.
+   * 
+   * @param {string} selector - The selector of the element to scroll to.
+   */
   inject('scrollToElement', (selector) => {
     if (typeof selector !== 'string' || selector.trim() === '') {
       throw new TypeError(
