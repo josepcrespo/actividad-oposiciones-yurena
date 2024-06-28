5e4216726aa261537f123c38fdcd493daf0c90e2
josepcs <josep.crespo.santacreu@gmail.com>
2024-06-28 18:07:01 +0100

    Added translations for "lastChallenge" and updated subtitle logic.

--- components/YrnTopMenuTitle.vue
+++ components/YrnTopMenuTitle.vue
@@ -1,32 +1,46 @@
 <i18n lang="yaml">
   ar:
     activityElement: "نشاط المدير"
+    lastChallenge: "التحدي الأخير"
   ca:
     activityElement: "Activitat del director"
+    lastChallenge: "Últim repte"
   de:
     activityElement: "Aktivität des Direktors"
+    lastChallenge: "Letzte Herausforderung"
   en:
     activityElement: "Director's activity"
+    lastChallenge: "Last challenge"
   es:
     activityElement: "Actividad del director"
+    lastChallenge: "Reto final"
   eu:
     activityElement: "Zuzendariaren jarduera"
+    lastChallenge: "Azken erronka"
   fr:
     activityElement: "Activité du directeur"
+    lastChallenge: "Dernier défi"
   gl:
     activityElement: "Actividade do director"
+    lastChallenge: "Último reto"
   it:
     activityElement: "Attività del direttore"
+    lastChallenge: "Ultima sfida"
   ja:
     activityElement: "ディレクターの活動"
+    lastChallenge: "最後の挑戦"
   pt:
     activityElement: "Atividade do diretor"
+    lastChallenge: "Último desafio"
   ro:
     activityElement: "Activitatea directorului"
+    lastChallenge: "Ultima provocare"
   ru:
     activityElement: "Директорская активность"
+    lastChallenge: "Последний вызов"
   zh:
     activityElement: "指导者活动"
+    lastChallenge: "最后的挑战"
 </i18n>
 
 <template>
@@ -65,11 +79,16 @@ export default {
 
       if (activityId && challengeId) {
           subtitle = this.$capitalizeWords(
-          // `${this.activityTranslation} ${activityId} / ` +
-          // The project owner required to set a static value for the activity.
-          `${this.$t('activityElement')} / ` +
-          `${this.challengeTranslation} ${challengeId}`
-        )
+            // `${this.activityTranslation} ${activityId} / ` +
+            // The project owner required to set a static value for the activity.
+            `${this.$t('activityElement')} / `
+          )
+          // eslint-disable-next-line eqeqeq
+          if (challengeId != 6) {
+            subtitle += `${this.challengeTranslation} ${challengeId}`
+          } else {
+            subtitle += this.$t('lastChallenge')
+          }
       } else {
         subtitle = this.$store?.state?.learningUnit?.indexPage?.subtitle?.[this.$i18n.locale]
           ?? ''
