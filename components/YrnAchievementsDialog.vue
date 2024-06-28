e23005ba697a39529b6229cda9d357379caa5ab3
josepcs <josep.crespo.santacreu@gmail.com>
2024-06-28 18:00:51 +0100

    New component, modal window with a list of achievements.

---
+++ components/YrnAchievementsDialog.vue
@@ -0,0 +1,253 @@
+<i18n lang="yaml">
+  ar:
+    dialogTitle: "الإنجازات"
+    exercisesCompleted: "التمارين المكتملة"
+    challengesCompleted: "التحديات المكتملة"
+    activitiesCompleted: "الأنشطة المكتملة"
+    errors: "الأخطاء"
+    errorsSubtitle: "كلما قلت كان أفضل!"
+    knowsTheSecretCode: "أنت تعرف الكود السري الذي يفتح النشاط التوجيهي!"
+    knowsTheSecretCodeSubtitle: "تذكر أن الكود يمكنك استخراجه من الرقم با (π)"
+  ca:
+    dialogTitle: "Premis"
+    exercisesCompleted: "Exercicis completats"
+    challengesCompleted: "Desafiaments completats"
+    activitiesCompleted: "Activitats completades"
+    errors: "Errors"
+    errorsSubtitle: "Com menys millor!"
+    knowsTheSecretCode: "Coneixes el codi secret que desbloqueja l'activitat del director!"
+    knowsTheSecretCodeSubtitle: "Recorda que el codi el pots extreure del número Pi (π)"
+  de:
+    dialogTitle: "Errungenschaften"
+    exercisesCompleted: "Abgeschlossene Übungen"
+    challengesCompleted: "Abgeschlossene Herausforderungen"
+    activitiesCompleted: "Abgeschlossene Aktivitäten"
+    errors: "Fehler"
+    errorsSubtitle: "Je weniger, desto besser!"
+    knowsTheSecretCode: "Du kennst den Geheimcode, der die Direktorenaktivität freischaltet!"
+    knowsTheSecretCodeSubtitle: "Denke daran, dass du den Code aus der Zahl Pi (π) extrahieren kannst"
+  en:
+    dialogTitle: "Achievements"
+    exercisesCompleted: "Exercises completed"
+    challengesCompleted: "Challenges completed"
+    activitiesCompleted: "Activities completed"
+    errors: "Errors"
+    errorsSubtitle: "The fewer, the better!"
+    knowsTheSecretCode: "You know the secret code that unlocks the director's activity!"
+    knowsTheSecretCodeSubtitle: "Remember that you can extract the code from the number Pi (π)"
+  es:
+    dialogTitle: "Logros"
+    exercisesCompleted: "Ejercicios completados"
+    challengesCompleted: "Desafíos completados"
+    activitiesCompleted: "Actividades completadas"
+    errors: "Errores"
+    errorsSubtitle: "¡Cuantos menos mejor!"
+    knowsTheSecretCode: "¡Conoces el código secreto que desbloquea la actividad del director!"
+    knowsTheSecretCodeSubtitle: "Recuerda que el código lo puedes extraer desde el número Pi (π)"
+  eu:
+    dialogTitle: "Arrakasta"
+    exercisesCompleted: "Osatutako ariketak"
+    challengesCompleted: "Amaitutako erronkak"
+    activitiesCompleted: "Amaitutako jarduerak"
+    errors: "Akatsak"
+    errorsSubtitle: "Zenbat eta gutxiago, hobeto!"
+    knowsTheSecretCode: "Zuzendariaren jarduera desblokeatzen duen kode sekretua dakizu!"
+    knowsTheSecretCodeSubtitle: "Gogoratu kodea Pi (π) zenbakitik atera dezakezula"
+  fr:
+    dialogTitle: "Réalisations"
+    exercisesCompleted: "Exercices complétés"
+    challengesCompleted: "Défis complétés"
+    activitiesCompleted: "Activités complétées"
+    errors: "Erreurs"
+    errorsSubtitle: "Le moins sera le mieux!"
+    knowsTheSecretCode: "Vous connaissez le code secret qui débloque l'activité du directeur!"
+    knowsTheSecretCodeSubtitle: "Souvenez-vous que vous pouvez extraire le code du nombre Pi (π)"
+  gl:
+    dialogTitle: "Logros"
+    exercisesCompleted: "Exercicios completados"
+    challengesCompleted: "Desafíos completados"
+    activitiesCompleted: "Actividades completadas"
+    errors: "Erros"
+    errorsSubtitle: "Cantos menos mellor!"
+    knowsTheSecretCode: "Coñeces o código secreto que desbloquea a actividade do director!"
+    knowsTheSecretCodeSubtitle: "Lembra que o código podes extraelo do número Pi (π)"
+  it:
+    dialogTitle: "Risultati"
+    exercisesCompleted: "Esercizi completati"
+    challengesCompleted: "Sfide completate"
+    activitiesCompleted: "Attività completate"
+    errors: "Errori"
+    errorsSubtitle: "Meno sono, meglio è!"
+    knowsTheSecretCode: "Conosci il codice segreto che sblocca l'attività del direttore!"
+    knowsTheSecretCodeSubtitle: "Ricorda che puoi estrarre il codice dal numero Pi (π)"
+  ja:
+    dialogTitle: "実績"
+    exercisesCompleted: "完了した演習"
+    challengesCompleted: "完了した挑戦"
+    activitiesCompleted: "完了したアクティビティ"
+    errors: "エラー"
+    errorsSubtitle: "少なければ少ないほど良い！"
+    knowsTheSecretCode: "ディレクターのアクティビティをアンロックする秘密のコードを知っています！"
+    knowsTheSecretCodeSubtitle: "コードは円周率（π）から抽出できることを忘れないでください"
+  pt:
+    dialogTitle: "Conquistas"
+    exercisesCompleted: "Exercícios concluídos"
+    challengesCompleted: "Desafios concluídos"
+    activitiesCompleted: "Atividades concluídas"
+    errors: "Erros"
+    errorsSubtitle: "Quanto menos, melhor!"
+    knowsTheSecretCode: "Você conhece o código secreto que desbloqueia a atividade do diretor!"
+    knowsTheSecretCodeSubtitle: "Lembre-se de que você pode extrair o código do número Pi (π)"
+  ro:
+    dialogTitle: "Realizări"
+    exercisesCompleted: "Exerciții finalizate"
+    challengesCompleted: "Provocări finalizate"
+    activitiesCompleted: "Activități finalizate"
+    errors: "Erori"
+    errorsSubtitle: "Cu cât mai puține, cu atât mai bine!"
+    knowsTheSecretCode: "Cunoști codul secret care deblochează activitatea directorului!"
+    knowsTheSecretCodeSubtitle: "Amintește-ți că poți extrage codul din numărul Pi (π)"
+  ru:
+    dialogTitle: "Достижения"
+    exercisesCompleted: "Завершенные упражнения"
+    challengesCompleted: "Завершенные задания"
+    activitiesCompleted: "Завершенные активности"
+    errors: "Ошибки"
+    errorsSubtitle: "Чем меньше, тем лучше!"
+    knowsTheSecretCode: "Вы знаете секретный код, который разблокирует активность директора!"
+    knowsTheSecretCodeSubtitle: "Помните, что вы можете извлечь код из числа Пи (π)"
+  zh:
+    dialogTitle: "成就"
+    exercisesCompleted: "完成的练习"
+    challengesCompleted: "完成的挑战"
+    activitiesCompleted: "完成的活动"
+    errors: "错误"
+    errorsSubtitle: "越少越好！"
+    knowsTheSecretCode: "你知道解锁董事活动的秘密代码！"
+    knowsTheSecretCodeSubtitle: "请记住，您可以从数字 Pi (π) 中提取代码"
+</i18n>
+
+<template>
+  <v-dialog
+    v-model="dialogModel"
+    transition="dialog-bottom-transition"
+    max-width="900"
+  >
+    <template #default>
+      <v-card class="yrn-achievements-dialog">
+        <v-toolbar
+          color="primary"
+          dark
+        >
+          <v-toolbar-title>{{ $t('dialogTitle') }}</v-toolbar-title>
+        </v-toolbar>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col
+                v-if="$store.getters['learningUnit/hasValidIndexPagePassword']"
+                cols="12"
+                class="d-flex flex-column"
+              >
+                <yrn-memoji
+                  class="mx-auto"
+                  :height="100"
+                  memoji-name="director-puño"
+                />
+                <p class="text-h6 text-center mb-0">
+                  {{ $t('knowsTheSecretCode') }}
+                </p>
+                <p class="subtitle-2 text-center font-italic font-weight-thin">
+                  {{ $t('knowsTheSecretCodeSubtitle') }}
+                </p>
+              </v-col>
+              <v-col class="d-flex flex-column">
+                <v-progress-circular
+                  class="mx-auto my-4"
+                  color="purple accent-4"
+                  :size="100"
+                  :value="7"
+                  :width="20"
+                >
+                  7
+                </v-progress-circular>
+                <p class="text-h6 text-center">
+                  {{ $t('exercisesCompleted') }}
+                </p>
+              </v-col>
+              <v-col class="d-flex flex-column">
+                <v-progress-circular
+                  class="mx-auto my-4"
+                  color="cyan accent-4"
+                  :size="100"
+                  :value="33"
+                  :width="20"
+                >
+                  33
+                </v-progress-circular>
+                <p class="text-h6 text-center">
+                  {{ $t('challengesCompleted') }}
+                </p>
+              </v-col>
+              <v-col class="d-flex flex-column">
+                <v-progress-circular
+                  class="mx-auto my-4"
+                  color="green accent-4"
+                  :size="100"
+                  :value="33"
+                  :width="20"
+                >
+                  33
+                </v-progress-circular>
+                <p class="text-h6 text-center">
+                  {{ $t('activitiesCompleted') }}
+                </p>
+              </v-col>
+              <v-col class="d-flex flex-column">
+                <v-progress-circular
+                  class="mx-auto my-4"
+                  color="orange accent-4"
+                  :size="100"
+                  :value="100"
+                  :width="20"
+                >
+                  0
+                </v-progress-circular>
+                <p class="text-h6 text-center mb-0">
+                  {{ $t('errors') }}
+                </p>
+                <p class="subtitle-2 text-center font-italic font-weight-thin">
+                  {{ $t('errorsSubtitle') }}
+                </p>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card-text>
+      </v-card>
+    </template>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: 'YrnAchievementsDialog',
+  computed: {
+    dialogModel: {
+      get() {
+        return this.$store?.state?.achievementsDialog
+      },
+      set(value) {
+        this.$store?.commit('setAchievementsDialog', value)
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.yrn-achievements-dialog {
+  .v-progress-circular {
+    font-family: monospace;
+  }
+}
+</style>
