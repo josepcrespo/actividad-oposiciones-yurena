c5424bf006f7ec2a3146307f8c9c008cad3aaa2c
josepcs <josep.crespo.santacreu@gmail.com>
2024-07-01 16:02:31 +0100

    Tabulation of the root content in i18n YAML structure allows to collapse the group on VSCode.

--- components/YrnNavigationDrawerRightToggle.vue
+++ components/YrnNavigationDrawerRightToggle.vue
@@ -1,46 +1,46 @@
 <i18n lang="yaml">
-es:
-  buttonTitle: "Progreso"
-  buttonTooltip: "Muestra el progreso en la Situación de Aprendizaje"
-ca:
-  buttonTitle: "Progrés"
-  buttonTooltip: "Mostra el progrés en la situació d'aprenentatge"
-en:
-  buttonTitle: "Progress"
-  buttonTooltip: "Show progress in the learning situation"
-de:
-  buttonTitle: "Fortschritt"
-  buttonTooltip: "Fortschritt in der Lernsituation anzeigen"
-ru:
-  buttonTitle: "Прогресс"
-  buttonTooltip: "Показать прогресс в учебной ситуации"
-fr:
-  buttonTitle: "Progrès"
-  buttonTooltip: "Afficher les progrès dans la situation d'apprentissage"
-it:
-  buttonTitle: "Progresso"
-  buttonTooltip: "Mostra il progresso nella situazione di apprendimento"
-eu:
-  buttonTitle: "Egiten"
-  buttonTooltip: "Erakutsi ikaskuntza-situan egindako aurrerapena"
-gl:
-  buttonTitle: "Progreso"
-  buttonTooltip: "Amosar o progreso na situación de aprendizaxe"
-pt:
-  buttonTitle: "Progresso"
-  buttonTooltip: "Mostrar o progresso na situação de aprendizagem"
-ar:
-  buttonTitle: "التقدم"
-  buttonTooltip: "عرض التقدم في الحالة التعلمية"
-zh:
-  buttonTitle: "进展"
-  buttonTooltip: "显示“学习情况”的进展"
-ro:
-  buttonTitle: "Progres"
-  buttonTooltip: "Arată progresul în situația de învățare"
-ja:
-  buttonTitle: "プログレソ"
-  buttonTooltip: "「学習状況」の進捗状況を表示します。"
+  es:
+    buttonTitle: "Progreso"
+    buttonTooltip: "Muestra el progreso en la Situación de Aprendizaje"
+  ca:
+    buttonTitle: "Progrés"
+    buttonTooltip: "Mostra el progrés en la situació d'aprenentatge"
+  en:
+    buttonTitle: "Progress"
+    buttonTooltip: "Show progress in the learning situation"
+  de:
+    buttonTitle: "Fortschritt"
+    buttonTooltip: "Fortschritt in der Lernsituation anzeigen"
+  ru:
+    buttonTitle: "Прогресс"
+    buttonTooltip: "Показать прогресс в учебной ситуации"
+  fr:
+    buttonTitle: "Progrès"
+    buttonTooltip: "Afficher les progrès dans la situation d'apprentissage"
+  it:
+    buttonTitle: "Progresso"
+    buttonTooltip: "Mostra il progresso nella situazione di apprendimento"
+  eu:
+    buttonTitle: "Egiten"
+    buttonTooltip: "Erakutsi ikaskuntza-situan egindako aurrerapena"
+  gl:
+    buttonTitle: "Progreso"
+    buttonTooltip: "Amosar o progreso na situación de aprendizaxe"
+  pt:
+    buttonTitle: "Progresso"
+    buttonTooltip: "Mostrar o progresso na situação de aprendizagem"
+  ar:
+    buttonTitle: "التقدم"
+    buttonTooltip: "عرض التقدم في الحالة التعلمية"
+  zh:
+    buttonTitle: "进展"
+    buttonTooltip: "显示“学习情况”的进展"
+  ro:
+    buttonTitle: "Progres"
+    buttonTooltip: "Arată progresul în situația de învățare"
+  ja:
+    buttonTitle: "プログレソ"
+    buttonTooltip: "「学習状況」の進捗状況を表示します。"
 </i18n>
 
 <template>
